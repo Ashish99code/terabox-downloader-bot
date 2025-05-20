985629bbed2af7daa1da404b886e7771dea46d32
Ashish99code <113297419+Ashish99code@users.noreply.github.com>
2025-05-20 19:53:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,13 @@ API_HASH = get_env_var("API_HASH")
 BOT_TOKEN = get_env_var("BOT_TOKEN")
 BOT_USERNAME = get_env_var("BOT_USERNAME")
 COOKIE = get_env_var("COOKIE")
-PRIVATE_CHAT_ID = 5083063115
-
+PRIVATE_CHAT_ID = int(os.getenv("PRIVATE_CHAT_ID", "5083063115"))  # Replace with your chat ID
+FORCE_LINK = os.getenv("FORCE_LINK", "@TERABOT_DOWNLOADER")  # Replace with your channel username
+PUBLIC_EARN_API = os.getenv("PUBLIC_EARN_API", "")  # Your PublicEarn API key
 
 # Redis Configuration
 HOST = os.getenv("REDIS_HOST", "localhost")
 PORT = int(os.getenv("REDIS_PORT", "6379"))
-PASSWORD = os.getenv("REDIS_PASSWORD", None)
+PASSWORD = os.getenv("REDIS_PASSWORD", "")
 
 ADMINS = [5083063115]  # Replace with your real Telegram user ID
